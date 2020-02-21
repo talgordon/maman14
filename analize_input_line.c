55738bb9ceb2be9f1ae7793bef65bb484e7e8a67
talgordon <talgordon0@gmail.com>
2020-02-21 06:24:26 -0500

    i added the method from maman 22

--- analize_input_line.c
+++ analize_input_line.c
@@ -19,6 +19,104 @@ uint64_t DC = 0;
 uint64_t IC = 0;
 uint16_t L = 0;
 
+/**skip white spaces.
+parameter: cmdLine - a pointer to a pointer to the input line, which is promote as long as the character in the line is a white space**/
+void skip_spaces(char **line)
+{
+	while((**line == ' ')||(**line == '\t'))
+		(*line)++;
+}
+
+/**check if the end of the line is legal - doesn't contain any characters which are not a white space
+
+parameter: cmdLine - a pointer to a pointer to the input line, to scan the end of the line.
+
+return: ERROR if the end of the line is ilegal, SUCCESS otherwise**/
+int legal_EOL(char ** line)
+{
+	while(**line != '\n') /**while it isn't the last character in the**/
+	{
+		if((**line == ' ')||(**line == '\t'))/**if it is a white space, move to the next character**/
+			(*line)++;
+		else /**if the character isn't a white space, error**/
+		{
+			printf("Extraneous text after end of commamd\n");
+			/**return ERROR, flag?, error handle?**/
+		}
+	}
+	/**return SUCCESS; leagl end of line**/
+}
+
+int comma_logic(char **line)
+{
+	skip_spaces(line);
+	if (**line != ',') /**check_comma**/
+	{
+		printf("Missing comma\n");
+		/**return error (flag), error handle**/
+	}
+	else /**found a comma**/
+	{
+		line++;
+		skip_spaces(line);
+		if (**line == ',') /**check for multiple commas**/
+			printf("Multiple consecutive commas\n");
+			/**return error (flag), error handle**/
+	}
+	/**succuss**/	
+}
+
+uint64_t get_data(char **line)
+{
+	char *pch; /**pointer which help to read a number every time (before a space or a comma) and promote cmdLine**/
+	char *num; /**pointer which contains the string representation of each number in the list.**/
+	int i, numLength, sign;
+	sign = 1;
+	numLength = 0;
+	num = (char *)malloc(10*sizeof(char));/**allocate place in the memory for num**/
+	while(**line != '\n')/**while the input line isn't finished, scan the list**/
+	{	
+		skip_spaces(line);
+		if(**line == '-') /**if there is a hyphen (maybe a minus sign) in the list of numbers**/
+		{
+			sign = -1;
+			(*line)++; /**move to the next character in the input**/
+		}
+		else /**the user didnt insert a hyphen**/
+		{
+			memset(num, '\0', strlen(num)); /**initialize the num pointer, to aviod junk cells**/
+			pch = strpbrk(*line," \t\n,"); /**find the first character in the input line which is a white space or a comma**/
+			strncpy(num, *line, (pch - *line)); /**insert the string which comes before the character in pch to num**/
+			*line = pch; /**promote cmdLine to after the string**/
+			numLength = strlen(num);/**save the length of the number which was found**/
+			/**scan all the string to check if it is a legal number**/			
+			for(i=0;i<numLength;i++,num++)
+			{
+				if(!isdigit(*num)) /**if the current character is not a digit, error*/
+				{
+					printf("Invalid data - not an integer\n");
+					/**return ERROR; flag, error handle**/
+				}	
+			}
+			
+		}
+		skip_spaces(line);
+		if (**line != ',') /**there is not a comma between two numbers, error**/
+		{
+			printf("Missing comma\n");
+			/**return ERROR;**/
+		}
+		(*line)++; /**move to the next character**/
+		skip_spaces(line);
+		if (**line == ',') /**multiple commas, error**/
+		{
+			printf("Multiple consecutive commas\n");
+			/**return ERROR;**/
+		}
+	}
+	return atoi(num)*sign;
+
+}
 bool line_first_analyzer(char * line)
 {
 	char * cut_line = line;
@@ -114,13 +212,5 @@ char * save_label(char * line)
 
 }
 
-char * ignore_spaces(char * line)
-{
-	int i = 0;
-	while(this_char == ' ' || this_char == "\t")
-	{
-		i++;
-		this_char = line[i];
-	}
-}
+
 

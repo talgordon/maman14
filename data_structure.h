a7270b76499bcc0f402491fceb84fbd034e6d63a
talgordon <talgordon0@gmail.com>
2020-03-05 11:03:54 -0500

    changes

--- data_structure.h
+++ data_structure.h
@@ -11,20 +11,22 @@ typedef struct PSW{
 	uint16_t ERROR : 1;
 	uint16_t LABEL : 1;
 	uint16_t Z : 1;
-}PSW;
-
+}PSW_hold;
+/**first code word**/
 typedef struct codeWord{
 	 uint16_t ARE : 3;
-	 uint16_t d : 4;
-	 uint16_t s : 4;
+	 uint16_t dst : 4;
+	 uint16_t src : 4;
 	 uint16_t opcode : 4;
 }codeWord;
 
+/**second/third code wird, a data word with direct/immidiate address method**/
 typedef struct infoWordData{
 	 uint16_t ARE : 3;
 	 uint16_t data : 12;
 }infoWordData;	
 
+/**second/third code wird, a data word with direct register/indirect register address method**/
 typedef struct infoWordReg{
 	 uint16_t ARE : 3;
 	 uint16_t srcReg : 3;
@@ -32,12 +34,28 @@ typedef struct infoWordReg{
 	 uint16_t rest : 6;
 }infoWordReg;
 
+typedef union u_ptr{
+	codeWord * codeWordPtr;
+	infoWordData * dataWordPtr;
+	infoWordReg * regWordPtr;
+}wordPtr;
+/**data word (data/string)**/
 typedef struct dataWord{
 	 uint16_t data : 15;
 }dataWord;
 
 typedef struct memWord* memWordPtr;
 
+/**a structure to hold a memory word, from one of the optional types/
+typedef union wordType{
+	codeWord c_word;
+	infoWordData info_d_word;
+	infoWordReg info_r_word;
+	dataWord d_word;
+
+}wordType;**/
+
+/**memory word, with the value and address (and the next word for the inked list)**/
 typedef struct memWord{
 	uint16_t word;
 	uint16_t index;
@@ -48,11 +66,12 @@ typedef struct error* errorPtr;
 
 typedef struct error{
 	uint16_t lineNum;
-	char * errorType;
+	int errorType;
 	errorPtr next;
 }error;
 
-const char * opcodeTable[16] = {"mov", "cmp", "add", "sub", "lea", "clr", "not", "inc", "dec", "jmp", "bne", "red", "prn", "jsr", "rts", "stop"};
+
+typedef struct addressMethod *addrMethodPtr;
 
 typedef struct addressMethod{
 	uint8_t src : 4;
@@ -61,8 +80,8 @@ typedef struct addressMethod{
 
 /**
 addressMethod addressTable[16];
-addressTable[0].src = 15;
-addressTable[0].dst = 7;
+addressTable[0]->src = 15;
+addressTable[0]->dst = 7;
 addressTable[1].src = 15; 
 addressTable[1].dst = 15; 
 addressTable[2].src = 15;
@@ -94,7 +113,6 @@ addressTable[14].dst = 0;
 addressTable[15].src = 0;
 addressTable[15].dst = 0; 
 **/
-
 typedef struct label * labelPtr;
 typedef struct label
 {

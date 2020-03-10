896fcef55100da91d2359e6b4d4d1b7b95924cce
talgordon <talgordon0@gmail.com>
2020-03-10 07:29:29 -0400

    changed

--- error.c
+++ error.c
@@ -1,53 +1,32 @@
-#include "flag.c"
-#define ERROR 1
-#define SUCCESS 0
-
-typedef struct error* errorPtr;
-
-typedef struct error{
-	uint16_t lineNum;
-	int errorType;
-	errorPtr next;
-}error;
-
-errorPtr head;
-enum errType{ALLOCATE, UNEXISTED};
-
-int line_num;
-int error_check(char *err);
-int add_error(int type);
-int print_error();
-   
-int error_check(char *err)
+#include "error.h"
+void error_check(char *err)
 {
 	set_flag("ERROR", 1);
 	if (strcmp(err, "ALLOCATE") == 0)
 		add_error(ALLOCATE);
 	if (strcmp(err, "UNEXISTED") == 0)
 		add_error(UNEXISTED);
-	return 0;
 }
 
-int add_error(int type)
+void add_error(int type)
 {
 	errorPtr tmp, newError;
-	if (head == NULL)
+	if (error_head == NULL)
 	{	
-		head = (errorPtr)malloc(sizeof(error));
-		if(head == NULL)
-			return ERROR;
-		head->errorType = type;
-		head->lineNum = line_num;
-		head->next = NULL;
-		return SUCCESS;
+		error_head = (errorPtr)malloc(sizeof(error));
+		if(error_head == NULL)
+			error_check("ALLOCATE");
+		error_head->errorType = type;
+		error_head->lineNum = line_num;
+		error_head->next = NULL;
 	}
 	else
 	{
 		tmp = (errorPtr)malloc(sizeof(error));
 		newError = (errorPtr)malloc(sizeof(error));
 		if((tmp == NULL)||(newError == NULL))
-			return ERROR;
-		tmp = head;
+			error_check("ALLOCATE");
+		tmp = error_head;
 		while(tmp->next != NULL)
 		{
 			tmp = tmp->next;
@@ -56,17 +35,16 @@ int add_error(int type)
 		newError->lineNum = line_num;
 		newError->next = NULL;
 		tmp->next = newError;
-		return SUCCESS;
 	}
 }
 
-int print_error()
+void print_error()
 {
 	errorPtr tmp;
 	tmp = (errorPtr)malloc(sizeof(error));
 	if(tmp == NULL)
-		return ERROR;
-	tmp = head;
+		error_check("ALLOCATE");
+	tmp = error_head;
 	while(tmp != NULL)
 	{
 		switch(tmp->errorType)
@@ -83,5 +61,36 @@ int print_error()
 		fprintf(stderr, ", in line:%d\n", tmp->lineNum);
 		tmp = tmp->next;	
 	}
-	return SUCCESS;
 }
+
+int get_flag(char * name)
+{
+	if (strcmp(name, "ERROR") == 0)
+		return PSW.ERR;
+	if (strcmp(name, "LABEL") == 0)
+		return PSW.LABEL;
+	if (strcmp(name, "Z") == 0)
+		return PSW.Z;
+	error_check("FLAG_NOT_EXIST");
+	return ERROR;
+}
+
+void set_flag(char *name, int value)
+{
+	if (strcmp(name, "ERROR") == 0)
+		PSW.ERR = value;
+	else
+	{
+		if (strcmp(name, "LABEL") == 0)
+			PSW.LABEL = value;
+		else
+		{   
+			if (strcmp(name, "Z") == 0)
+				PSW.Z = value;	
+			else
+				error_check("FLAG_NOT_EXIST");
+		}
+	}
+}
+
+

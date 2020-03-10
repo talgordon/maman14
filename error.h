ea384b01b3b4eab5e05ce4bac64de6b549679210
talgordon <talgordon0@gmail.com>
2020-03-10 07:31:33 -0400

    added

---
+++ error.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ERROR -1
+#define SUCCESS 0
+
+typedef struct PSW{
+	unsigned int ERR : 1;
+	unsigned int LABEL : 1;
+	unsigned int Z : 1;
+}PSW_hold;
+
+typedef struct error* errorPtr;
+
+typedef struct error{
+	unsigned int lineNum;
+	unsigned int errorType;
+	errorPtr next;
+}error;
+
+errorPtr error_head;
+enum errType{ALLOCATE, UNEXISTED, FLAG_NOT_EXIST,CANNOT_OPEN_FILE, LINE_INVALID, INVALID_DATA};
+	
+PSW_hold PSW;
+int line_num;
+void error_check(char *err);
+void add_error(int type);
+void print_error();
+int get_flag(char * name);
+void set_flag(char *name, int value);

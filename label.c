f5d579385174368a1770d9cbf7258599a1704a5b
talgordon <talgordon0@gmail.com>
2020-03-10 07:29:57 -0400

    changed

--- label.c
+++ label.c
@@ -1,63 +1,46 @@
-#include "data_structure.h"
-#include "error.c"
+#include "label.h"
 
-enum status{SUCCESS, ERROR};
-enum labeltypes{UNDEFINED, DATA, ENTRY, EXTERN, CODE};
-enum labelFields{NAME, VALUE, TYPE};
-
-labelPtr head;
-
-int add_label(char * name, int value, int type);
-int get_label(char *name, int feature, int featureType, labelPtr* label);
-int update_label(int update, int updateType, char *name);
-int print_label();
-
-
-int print_label()
+void print_label()
 {
 	labelPtr tmp;
 	tmp = (labelPtr)malloc(sizeof(label));
 	if(tmp == NULL)
 	{
 		error_check("ALLOCATE");
-		return ERROR;
 	}
-	tmp = head;
+	tmp = label_head;
 	while(tmp != NULL)
 	{
-		printf("\n%s, %d, ", tmp->labelName, tmp->value);
-		if(tmp->LabelType == DATA)
+		printf("\n%s, %d, ", tmp->labelName, tmp->labelValue);
+		if(tmp->labelType == DATA)
 			printf("data\n");
-		if(tmp->LabelType == ENTRY)
+		if(tmp->labelType == ENTRY)
 			printf("entry\n");
-		if(tmp->LabelType == EXTERN)
+		if(tmp->labelType == EXTERN)
 			printf("extern\n");
-		if(tmp->LabelType == CODE)
+		if(tmp->labelType == CODE)
 			printf("code\n");
-		if(tmp->LabelType == UNDEFINED)
+		if(tmp->labelType == UNDEFINED)
 			printf("undefined\n");
 		tmp = tmp->next;	
 	}
-	return SUCCESS;
 }
 
-int add_label(char * name, int value, int type)
+void add_label(char * name, int value, int type)
 {
 	labelPtr tmp, newLabel;
-	if (head == NULL)
+	if (label_head == NULL)
 	{	
-		head = (labelPtr)malloc(sizeof(label));
-		head->labelName = (char *)malloc(sizeof(name)*sizeof(char));
-		if((head->labelName == NULL)||(head == NULL))
+		label_head = (labelPtr)malloc(sizeof(label));
+		label_head->labelName = (char *)malloc(sizeof(name)*sizeof(char));
+		if((label_head->labelName == NULL)||(label_head == NULL))
 		{
-			check_error("ALLOCATE");
-			return ERROR;
+			error_check("ALLOCATE");
 		}
-		strcpy(head->labelName, name);
-		head->value = value;
-		head->LabelType = type;
-		head->next = NULL;
-		return SUCCESS;
+		strcpy(label_head->labelName, name);
+		label_head->labelValue = value;
+		label_head->labelType = type;
+		label_head->next = NULL;
 	}
 	else
 	{
@@ -65,54 +48,50 @@ int add_label(char * name, int value, int type)
 		newLabel = (labelPtr)malloc(sizeof(label));
 		if((tmp == NULL)||(newLabel == NULL))
 		{
-			check_error("ALLOCATE");
-			return ERROR;
+			error_check("ALLOCATE");
 		}
-		tmp = head;
+		tmp = label_head;
 		while(tmp->next != NULL)
 		{
 			if(strcmp(tmp->labelName,name) == 0)
 			{
-				check_error("EXISTED");
-				return ERROR;
+				error_check("EXISTED");
 			}
 			tmp = tmp->next;
 		}
 		newLabel->labelName = (char *)malloc(sizeof(name)*sizeof(char));
 		if (newLabel->labelName == NULL)
 		{
-			check_error("ALLOCATE");
-			return ERROR;
+			error_check("ALLOCATE");
 		}
 		strcpy(newLabel->labelName, name);
-		newLabel->value = value;
-		newLabel->LabelType = type;
+		newLabel->labelValue = value;
+		newLabel->labelType = type;
 		newLabel->next = NULL;
 		tmp->next = newLabel;
-		return SUCCESS;
 	}
 }
 
-int get_label(char *name, int feature, int featureType, labelPtr* label)
+void get_label(char *name, int feature, int featureType, labelPtr* label)
 {
 	if(*label == NULL)
 	{
-		check_error("ALLOCATE");
-		return ERROR;
+		error_check("ALLOCATE");
+		return;
 	}
-	*label = head;
+	*label = label_head;
 	while(*label != NULL)
 	{
-		if((featureType == NAME) && (strcmp((*label)->labelName,name) == 0))	
-			return SUCCESS;
-		if((featureType == VALUE) && (feature == (*label)->value))	
-			return SUCCESS;
-		if((featureType == TYPE) && (feature == (*label)->LabelType))
-			return SUCCESS;
+		if((featureType == LABEL_NAME) && (strcmp((*label)->labelName,name) == 0))	
+			return;
+		if((featureType == LABEL_VALUE) && (feature == (*label)->labelValue))	
+			return;
+		if((featureType == LABEL_TYPE) && (feature == (*label)->labelType))
+			return;
 		*label = (*label)->next;
 	}
+	*label = NULL;
 	error_check("UNEXISTED");
-	return ERROR;
 }
 
 int update_label(int update, int updateType, char *name)
@@ -121,22 +100,22 @@ int update_label(int update, int updateType, char *name)
 	tmp = (labelPtr)malloc(sizeof(label));
 	if(tmp == NULL)
 	{
-		check_error("ALLOCATE");
+		error_check("ALLOCATE");
 		return ERROR;
 	}	
-	tmp = head;
+	tmp = label_head;
 	while(tmp != NULL)
 	{
 		if(strcmp(tmp->labelName, name) == 0)
 		{
-			if(updateType == VALUE)
+			if(updateType == LABEL_VALUE)
 			{
-				tmp->value = update;
+				tmp->labelValue = update;
 				return SUCCESS;
 			}
-			if(updateType == TYPE)
+			if(updateType == LABEL_TYPE)
 			{
-				tmp->LabelType = update;
+				tmp->labelType = update;
 				return SUCCESS;
 			}		
 		}

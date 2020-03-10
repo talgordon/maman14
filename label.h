b9b4896ad4c8e01ec083c2ff287cb57970846d70
talgordon <talgordon0@gmail.com>
2020-03-10 07:31:55 -0400

    added

---
+++ label.h
@@ -0,0 +1,22 @@
+#include "error.h"
+
+typedef struct label * labelPtr;
+typedef struct label
+{
+	char * labelName;
+	unsigned int labelValue;
+	unsigned int labelType;
+	labelPtr next;
+}label;
+
+
+enum labeltypes{UNDEFINED_LABEL, DATA_LABEL, ENTRY_LABEL, EXTERN_LABEL, CODE_LABEL};
+enum labelFields{LABEL_NAME, LABEL_VALUE, LABEL_TYPE};
+
+labelPtr label_head;
+
+void add_label(char * name, int value, int type);
+void get_label(char *name, int feature, int featureType, labelPtr* label);
+int update_label(int update, int updateType, char *name);
+void print_label();
+

755b2e1e97fc089f4c161034d856940526124b01
talgordon <talgordon0@gmail.com>
2020-03-16 13:53:00 -0400

    work

--- analize_code_word.c
+++ analize_code_word.c
@@ -1,5 +1,6 @@
-#include "translate.c"
+#include "analize_code_word.h"
 #include "analize_input_line.h"
+#include "error.h"
 #define MAX_WORDS 4096
 #define A 4
 #define R 2
@@ -10,39 +11,6 @@
 #define DIRECT_REGISTER 8
 
 const char * opcodeTable[16] = {"mov", "cmp", "add", "sub", "lea", "clr", "not", "inc", "dec", "jmp", "bne", "red", "prn", "jsr", "rts", "stop"};
-/**addressMethod addressTable[16];
-addressTable[0]->src = 15;
-addressTable[0]->dst = 7;
-addressTable[1].src = 15; 
-addressTable[1].dst = 15; 
-addressTable[2].src = 15;
-addressTable[2].dst = 7;
-addressTable[3].src = 15;
-addressTable[3].dst = 7;
-addressTable[4].src = 4;
-addressTable[4].dst = 7; 
-addressTable[5].src = 0;
-addressTable[5].dst = 7; 
-addressTable[6].src = 0;
-addressTable[6].dst = 7; 
-addressTable[7].src = 0;
-addressTable[7].dst = 7; 
-addressTable[8].src = 0;
-addressTable[8].dst = 7; 
-addressTable[9].src = 0;
-addressTable[9].dst = 6; 
-addressTable[10].src = 0;
-addressTable[10].dst = 6;    
-addressTable[11].src = 0;
-addressTable[11].dst = 7;  
-addressTable[12].src = 0;
-addressTable[12].dst = 15; 
-addressTable[13].src = 0;
-addressTable[13].dst = 6;  
-addressTable[14].src = 0;
-addressTable[14].dst = 0;
-addressTable[15].src = 0;
-addressTable[15].dst = 0; **/
 
 int find_opcode(char * opcode)
 {

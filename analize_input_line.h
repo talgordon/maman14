df73e06d0cd170fe1d810882543080bec28bd1cd
talgordon <talgordon0@gmail.com>
2020-03-16 13:53:23 -0400

    work

--- analize_input_line.h
+++ analize_input_line.h
@@ -1,4 +1,3 @@
-
 #include <ctype.h>
 	
 unsigned int DC = 0;
@@ -7,12 +6,22 @@ unsigned int L = 0;
 
 typedef enum type{LABEL , DATA , STRING , EXTERN , ENTRY, OTHER, END} types;
 
+typedef struct addressMethod *addrMethodPtr;
+
+typedef struct addressMethod{
+	unsigned int src : 4;
+	unsigned int dst : 4;
+}addressMethod;
+
+addressMethod addressTable[16];
+
 void get_line(int argc, char * argv[]);
 void skip_spaces(char **line);
 void legal_EOL(char ** line);
 void comma_logic(char **line);
+void init();
 int get_data(char **line);
 int has_label(char * line);
-types get_word(char ** line, char ** word);
+types get_word(char * line[], char ** word);
 void get_operand(char * line, int * srcType, int *dstType, char ** srcName, char ** dstName);
 

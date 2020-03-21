0dca77ddb02a7bfec3586fb7a2d93920eee38149
user <user@ubuntu.(none)>
2020-03-21 18:38:20 -0400

    new

--- build.c
+++ build.c
@@ -1,30 +1,34 @@
-
 #include "analize_code_word.h"
 #include "build.h"
 #include "label.h"
 #include "analize_input_line.h"
+
+/*A function that creates the program output files by calling to build_object, build_entry and build_extern*/
 void build_output()
 {
 	build_object();
 	build_entry();
 	build_extern();
 }
+
+/*A function that creates the program output entry files*/
 void build_entry()
 {
 	labelPtr label;
 	FILE *fp;
 	label = (labelPtr)malloc(sizeof(label));
 	label = label_head;
-	fp = fopen("ps.ent", "w");
-	while(label!=NULL)
+	fp = fopen("ps.ent", "w");/*Open the file ps.ent*/
+	while(label!=NULL)/*Run as long as label equal to NULL and print the label name and his value.*/
 	{
-		if(label->labelType == ENTRY_LABEL)
+		if(label->labelType == ENTRY_LABEL)/*Check if is entry-print*/
 			fprintf(fp,"%s\t%d\n", label->labelName, label->labelValue);
-		label = label->next;
+		label = label->next;/*If not a entry-one step ahead*/
 	}
-	fclose(fp);
+	fclose(fp);/*Close the file*/
 }
 
+/*A function that creates the program output extern files*/
 void build_extern()
 {
 	labelPtr label;
@@ -33,26 +37,28 @@ void build_extern()
 	mask = 1;
 	label = (labelPtr)malloc(sizeof(label));
 	label = label_head;
-	fp = fopen("ps.ext", "w");
-	for(i = 0; i < IC; i++)
+	fp = fopen("ps.ext", "w");/*Open the file ps.ext*/
+	for(i = 0; i < IC; i++)/*A loop that run on all the IC*/
 	{
 		if(buffer[i]&mask)
 		{
-			while(label!=NULL)
+			while(label!=NULL)/*Run as long as label equal to NULL and print the label name and his place.*/
 			{
-				if((label->labelType == EXTERN_LABEL)&&(label->labelValue == buffer[i]>>2))
+				if((label->labelType == EXTERN_LABEL)&&(label->labelValue == buffer[i]>>2))/*If is extern-print*/
 					fprintf(fp,"%s\t%d\n", label->labelName, i);
-				label = label->next;
+				label = label->next;/*If not a extern-one step ahead*/
 			}
 		}
 	}
-	fclose(fp);
+	fclose(fp);/*Close the file*/
 }
+
+/*A function that creates the program output object files*/
 void build_object()
 {	
 	FILE *fp;
-	fp = fopen("ps.ob", "w");
-	fprintf(fp,"\n\t%d,\t%d\n", IC, DC);
-	print_mem();
-	fclose(fp);
+	fp = fopen("ps.ob", "w");/*Open the file ps.ob*/
+	fprintf(fp,"\n\t%d,\t%d\n", IC, DC);/*Print the IC and DC*/
+	print_mem();/*Call to the function print_mem that print ????*/
+	fclose(fp);/*Close the file*/
 }

ec2d17052706f9690a674702544d6c4eb6098af8
talgordon <talgordon0@gmail.com>
2020-03-23 04:13:47 -0400

    work

--- label.h
+++ label.h
@@ -1,21 +1,26 @@
 #define NO_ADDRESS 0
+#define MAX_LABEL 31
 typedef struct label * labelPtr;
 typedef struct label
 {
 	char * labelName;
 	unsigned int labelValue;
 	unsigned int labelType;
+	unsigned int labelLink;
 	labelPtr next;
 }label;
 
 
-enum labeltypes{UNDEFINED_LABEL, DATA_LABEL, ENTRY_LABEL, EXTERN_LABEL, CODE_LABEL};
-enum labelFields{LABEL_NAME, LABEL_VALUE, LABEL_TYPE};
+enum labeltypes{UNDEFINED_LABEL, DATA_LABEL, CODE_LABEL};
+enum labelLinks{NOT_LINKED_LABEL, EXTERN_LABEL, ENTRY_LABEL};
+enum labelFields{LABEL_NAME, LABEL_VALUE, LABEL_TYPE, LABEL_LINK};
 
 labelPtr label_head;
 
-void add_label(char * name, int value, int type);
+int isreserved(char * word);
+int isillegal(char * word);
+void add_label(char * name, int value, int type, int link);
 void get_label(char *name, int feature, int featureType, labelPtr* label);
-int update_label(int update, int updateType, int labelValue, char *name);
+int update_label(int update, int updateType, int labelID, int IDType, char *name);
 void print_label();
 

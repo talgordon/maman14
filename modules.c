fe2f90915e22cf9fd51efeb7db8b569b39ac9e09
shira rath <shirarath@gmail.com>
2020-03-25 17:40:36 -0400

    work

--- modules.c
+++ modules.c
@@ -1,47 +1,41 @@
-
 #include "analize_input_line.h"
 #include "label.h"
 #include "error.h"
 #include "analize_code_word.h"
 #include "modules.h"
 
+/*A function that handles a guidance that has data*/
 void data_handle(char * labelName, int type)
 {
-	printf("in data_handle\n");
-	if (get_flag("LABEL") == 1)
+	if (get_flag("LABEL") == 1)/*If have label*/
 	{
-		add_label(labelName, DC, DATA_LABEL, NOT_LINKED_LABEL);
+		add_label(labelName, DC, DATA_LABEL, NOT_LINKED_LABEL);/*Add the label to the linked list*/
 		set_flag("LABEL", 0);
-		printf("in data_handle, label %s was added\n", labelName);
 	}
-	printf("in data_handle, call translate data with %s\n", inputLine);
-	translate_data(type, inputLine);
-	printf("finish translate_data\n");
-	
+	translate_data(type, inputLine);/*Call to translate_data to encode to binary*/
 }
 
+/*A function that handles a guidance that has entry*/
 void entry_handle()/*second loop*/
 {
-	char *word;
+	char * word;
 	word = (char *)malloc(sizeof(char)*MAX_WORD);
-	printf("in entry handle, buf is:%s\n", inputLine);
 	while (get_word(&inputLine, &word)!= END)
-		update_label(ENTRY_LABEL, LABEL_LINK, 0, 0, word);
+		update_label(ENTRY_LABEL, LABEL_LINK, 0, 0, word);/*Updating a label in the linked list*/
 }
 
+/*A function that handles a guidance that has extern*/
 void extern_handle()
 {
 	char *word;
 	word = (char *)malloc(sizeof(char)*MAX_WORD);
-	printf("in extern handle, buf is:%s\n", inputLine);
 	while (get_word(&inputLine, &word)!= END)
 	{
-		printf("label to add:%s\n", word);
-		add_label(word, NO_ADDRESS, UNDEFINED_LABEL, EXTERN_LABEL);
-		printf("label %s was added\n", word);
+		add_label(word, NO_ADDRESS, UNDEFINED_LABEL, EXTERN_LABEL);/*Add the label to the linked list*/
 	}
 }
 
+/*A function that handles insruction*/
 void code_handle_first(char * labelName, char * word)
 {
 	int srcType, dstType, opcode;
@@ -50,35 +44,32 @@ void code_handle_first(char * labelName, char * word)
 	wordPtr wPtr;
 	srcName = (char *)malloc(sizeof(char)*MAX_WORD);
 	dstName = (char *)malloc(sizeof(char)*MAX_WORD);
-	printf("in code handle, buf is:%s\n", inputLine);
-	if (get_flag("LABEL") == 1)
+	if (get_flag("LABEL") == 1)/*If have label*/
 	{
-		add_label(labelName, IC, CODE_LABEL, NOT_LINKED_LABEL);
+		add_label(labelName, IC, CODE_LABEL, NOT_LINKED_LABEL);/*Add the label to the linked list*/
 		set_flag("LABEL", 0);
-		printf("label %s was added\n", labelName);
 	}
-	if ((opcode = find_opcode(word)) < 0)
+	if ((opcode = find_opcode(word)) < 0)/*If the opcode is illegal, error*/
 		add_error(UNEXISTED_OPCODE);
-	printf("line for get_operands:%s\n", inputLine);
-	get_operand(inputLine, &srcType, &dstType, &srcName, &dstName, 1);
+
+	get_operand(inputLine, &srcType, &dstType, &srcName, &dstName, 1);/*Call to get_operand*/
 	L = 1;
-	if (dstType != 0)/*change to handle a case when two operands are represnted in 1 info word*/
+	if (dstType != 0)
 	{
 		L++;
 		if (!((srcType == DIRECT_REGISTER || srcType == INDIRECT_REGISTER) && (dstType == DIRECT_REGISTER || dstType == INDIRECT_REGISTER)) && (srcType != 0))
 			L++;
 	}
-	printf("opcode:%d, srcType:%d, dstType:%d, srcName:%s, dstName:%s, L:%d\n", opcode, srcType, dstType, srcName, dstName, L);
+	
 	translate_code(wPtr, opcode, srcType, dstType);
 	IC = IC + L;
 	
 }
+
+/*A function that call to finish_translate to finish a translate the input line*/
 void code_handle_second()
 {
 	wordPtr wPtr;
-	printf("in second code handle, buf:%s\n", inputLine);
 	IC++;
 	finish_translate(inputLine, wPtr);
-	printf("finish finish translate\n");
-	printf("IC:%d\n", IC);	
 }

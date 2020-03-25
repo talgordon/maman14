e224be22b559f7a5128ff3ff50f2e73b2199c9ce
shira rath <shirarath@gmail.com>
2020-03-25 17:41:39 -0400

    work

--- modules.h
+++ modules.h
@@ -1,6 +1,12 @@
-void data_handle();
+/*A function that handles a guidance that has data*/
+void data_handle(char * labelName, int type);
+/*A function that handles a guidance that has extern*/
 void extern_handle();
+/*A function that handles a guidance that has entry*/
 void entry_handle();
-void code_handle();
-void label_handle(char ** labelName);
+/*A function that handles insruction*/
+void code_handle_first(char * labelName, char * word);
+/*A function that call to finish_translate to finish a translate the input line*/
+void code_handle_second();
+
 

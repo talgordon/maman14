8fed60321e0d83e378c7c95215e7f5869ebaed6f
talgordon <talgordon0@gmail.com>
2020-03-25 06:25:14 -0400

    final

--- modules.c
+++ modules.c
@@ -14,8 +14,8 @@ void data_handle(char * labelName, int type)
 		set_flag("LABEL", 0);
 		printf("in data_handle, label %s was added\n", labelName);
 	}
-	printf("in data_handle, call translate data with %s\n", buf[line_num]);
-	translate_data(type, buf[line_num]);
+	printf("in data_handle, call translate data with %s\n", inputLine);
+	translate_data(type, inputLine);
 	printf("finish translate_data\n");
 	
 }
@@ -24,8 +24,8 @@ void entry_handle()/*second loop*/
 {
 	char *word;
 	word = (char *)malloc(sizeof(char)*MAX_WORD);
-	printf("in entry handle, buf[line_num] is:%s\n", buf[line_num]);
-	while (get_word(&buf[line_num], &word)!= END)
+	printf("in entry handle, buf is:%s\n", inputLine);
+	while (get_word(&inputLine, &word)!= END)
 		update_label(ENTRY_LABEL, LABEL_LINK, 0, 0, word);
 }
 
@@ -33,8 +33,8 @@ void extern_handle()
 {
 	char *word;
 	word = (char *)malloc(sizeof(char)*MAX_WORD);
-	printf("in extern handle, buf[line_num] is:%s\n", buf[line_num]);
-	while (get_word(&buf[line_num], &word)!= END)
+	printf("in extern handle, buf is:%s\n", inputLine);
+	while (get_word(&inputLine, &word)!= END)
 	{
 		printf("label to add:%s\n", word);
 		add_label(word, NO_ADDRESS, UNDEFINED_LABEL, EXTERN_LABEL);
@@ -50,7 +50,7 @@ void code_handle_first(char * labelName, char * word)
 	wordPtr wPtr;
 	srcName = (char *)malloc(sizeof(char)*MAX_WORD);
 	dstName = (char *)malloc(sizeof(char)*MAX_WORD);
-	printf("in code handle, buf[line_num] is:%s\n", buf[line_num]);
+	printf("in code handle, buf is:%s\n", inputLine);
 	if (get_flag("LABEL") == 1)
 	{
 		add_label(labelName, IC, CODE_LABEL, NOT_LINKED_LABEL);
@@ -59,8 +59,8 @@ void code_handle_first(char * labelName, char * word)
 	}
 	if ((opcode = find_opcode(word)) < 0)
 		add_error(UNEXISTED_OPCODE);
-	printf("line for get_operands:%s\n", buf[line_num]);
-	get_operand(buf[line_num], &srcType, &dstType, &srcName, &dstName, 1);
+	printf("line for get_operands:%s\n", inputLine);
+	get_operand(inputLine, &srcType, &dstType, &srcName, &dstName, 1);
 	L = 1;
 	if (dstType != 0)/*change to handle a case when two operands are represnted in 1 info word*/
 	{
@@ -76,9 +76,9 @@ void code_handle_first(char * labelName, char * word)
 void code_handle_second()
 {
 	wordPtr wPtr;
-	printf("in second code handle, buf[line_num]:%s\n", buf[line_num]);
+	printf("in second code handle, buf:%s\n", inputLine);
 	IC++;
-	finish_translate(buf[line_num], wPtr);
+	finish_translate(inputLine, wPtr);
 	printf("finish finish translate\n");
 	printf("IC:%d\n", IC);	
 }

82646a9a0da986a7863c2c3278c5bf7b22b1639a
shira rath <shirarath@gmail.com>
2020-03-25 17:46:55 -0400

    work

--- build.h
+++ build.h
@@ -1,5 +1,9 @@
+/*A function that creates the program output entry files*/
 void build_entry();
+/*A function that creates the program output extern files*/
 void build_extern();
+/*A function that creates the program output object files*/
 void build_object();
+/*A function that creates the program output files by calling to build_object, build_entry and build_extern*/
 void build_output();
 

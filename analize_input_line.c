177de7ec64b734fe58a0e610f0bb016ab714cd8f
talgordon <talgordon0@gmail.com>
2020-03-27 07:27:13 -0400

    final

--- analize_input_line.c
+++ analize_input_line.c
@@ -1,52 +1,7 @@
 #include "analize_input_line.h"
 #include "error.h"
 #include "label.h"
-
-/*A function that initializes global variables using code*/
-void init()
-{
-	int i;
-	addressTable[0].src = 15;
-	addressTable[0].dst = 7;
-	addressTable[1].src = 15; 
-	addressTable[1].dst = 15; 
-	addressTable[2].src = 15;
-	addressTable[2].dst = 7;
-	addressTable[3].src = 15;
-	addressTable[3].dst = 7;
-	addressTable[4].src = 4;
-	addressTable[4].dst = 7; 
-	addressTable[5].src = 0;
-	addressTable[5].dst = 7; 
-	addressTable[6].src = 0;
-	addressTable[6].dst = 7; 
-	addressTable[7].src = 0;
-	addressTable[7].dst = 7; 
-	addressTable[8].src = 0;
-	addressTable[8].dst = 7; 
-	addressTable[9].src = 0;
-	addressTable[9].dst = 6; 
-	addressTable[10].src = 0;
-	addressTable[10].dst = 6;    
-	addressTable[11].src = 0;
-	addressTable[11].dst = 7;  
-	addressTable[12].src = 0;
-	addressTable[12].dst = 15; 
-	addressTable[13].src = 0;
-	addressTable[13].dst = 6;  
-	addressTable[14].src = 0;
-	addressTable[14].dst = 0;
-	addressTable[15].src = 0;
-	addressTable[15].dst = 0;
-	
-	inputLine = (char *)malloc(sizeof(char)*MAX_LINE);
-
-	line_num = 0;
-	IC = 100;
-	DC = 0;
-	L = 0;
-}
-
+#include "modules.h"
 /*A function that skip white spaces.*/
 void skip_spaces(char **line)
 {
@@ -92,7 +47,7 @@ int get_data(char **line)
 {
 	char *pch; /*pointer which help to read a number every time (before a space or a comma) and promote cmdLine*/
 	char *num; /*pointer which contains the string representation of each number in the list.*/
-	int i, numLength, sign;
+	int i, numLength, sign, data;
 	sign = 1;
 	numLength = 0;
 	num = (char *)malloc(10*sizeof(char));/**allocate place in the memory for num**/
@@ -113,7 +68,7 @@ int get_data(char **line)
 		numLength = strlen(num);/*save the length of the number which was found*/
 		/*scan all the string to check if it is a legal number*/
 				
-		for(i=0;i<numLength;i++)
+		for(i = 0; i < numLength; i++)
 		{
 			if(!isdigit(num[i])) /*if the current character is not a digit, error*/
 			{
@@ -132,11 +87,15 @@ int get_data(char **line)
 		{
 			add_error(MULTIPLE_COMMA);
 		}
-
-		return (atoi(num)*sign);
+		data = (atoi(num)*sign);
+		free(num);
+		return data;
 	}
 	else
+	{
+		free(num);
 		return EOF;
+	}
 }
 
 /*A function that check if the line is label and return 0/1*/
@@ -147,15 +106,15 @@ int is_label(char * line)
 		if (*line == ':')/*If is label*/
 		{
 			if(isspace(*(++line)))
-				return 0;	
+				return IS_TRUE;	
 		}
 		line++;
 	}
-	return 1;/*if is not a label*/
+	return IS_FALSE;/*if is not a label*/
 }
 
 /*A function that analize an input line. Reads a first word from the line to the space and returns an enum according to the type it has inserted*/
-types get_word(char * line[40], char ** word)
+types get_word(char * line[MAX_LINE], char **word)
 {
 	
 	char * pch;
@@ -165,7 +124,7 @@ types get_word(char * line[40], char ** word)
 	{
 		return END;	
 	}
-	if((is_label(*line)) == 0)/*Check if is label-return LABEL*/
+	if(is_label(*line))/*Check if is label-return LABEL*/
 	{
 		pch=(strpbrk(*line," \t\n,"));
 		strncpy(*word,*line,(pch-*line));
@@ -208,137 +167,135 @@ void get_operand(char * line, int *srcType, int *dstType, char ** srcName, char 
 	labelPtr label;
 	char * pch;
 	char * str;
-	str = (char *)malloc(sizeof(char)*MAX_WORD);
-	memset(str, '\0', strlen(str));
-	label=(labelPtr)malloc(sizeof(label));
 	skip_spaces(&line);	
 	if (*line == '\n')/*If the beginning of the line is empty, no operands*/
 	{
-		*srcType = 0;
+		*srcType = NO_OPERAND;
 		*srcName = NULL;
-		*dstType = 0;
+		*dstType = NO_OPERAND;
 		*dstName = NULL;
-		return;
 	}
-	pch=(strpbrk(line," \t\n,"));
-	strncpy(str,line,(pch-line));
-	line=pch;
-	skip_spaces(&line);
-	if (*line == '\n')
+	else
 	{
-		*srcType = 0;
-		*srcName = NULL;
-	}
-	else/*If its have a operands*/
-	{
-		comma_logic(&line);/*Checks if its have a comma*/
-		skip_spaces(&line);
-		switch(*str)/*Switch that checks the first operand type and copy the type to dstName*/
-		{
-			case '#':/*If is direct address-initializes the srcType accordingly*/
-			{
-				*srcType=1;
-				strcpy(*srcName, (str+1));
-				break;
-			}
-
-			case '*':/*If is indirect address-initializes the srcType accordingly*/
-			{	
-
-			    	*srcType=4;
-				strcpy(*srcName, (str+2));
-				break;
-			}
-	 		
-			default :
-			{
-				*srcType=4;
-				if(*str == 'r')/*If is direct register address-initializes the srcType accordingly*/
-				{
-					str++;
-					if ((atoi(str) < 8) && (atoi(str) > 0))
-					{
-						if(*(str+1) == '\0')
-						{
-							*srcType = 8;
-							strcpy(*srcName, str);
-						}
-					}
-				}
-				else/*If is not one of the previous cases*/
-				{
-					if (loop == 2)
-						get_label(str, 0, 0, &label);
-					if((label!=NULL)||(loop == 1))/*Is label*/
-					{
-						*srcType = 2;
-						strcpy(*srcName, str);
-					}
-					else/*If is not of the previous cases,error*/
-					{
-						add_error(INVALID_SRC_TYPE);
-					}
-				}
-				break;
-			}
-		}
+		str = (char *)malloc(sizeof(char)*MAX_WORD);
 		memset(str, '\0', strlen(str));
 		pch=(strpbrk(line," \t\n,"));
 		strncpy(str,line,(pch-line));
 		line=pch;
-	}
-	
-	switch(*str)/*A same switch that checks the secand operand type and copy the type to dstName*/
-	{
-		case '#':
+		skip_spaces(&line);
+		if (*line == '\n')
 		{
-			*dstType=1;
-			strcpy(*dstName, (str+1));		
-			break;
+			*srcType = NO_OPERAND;
+			*srcName = NULL;
 		}
-		case '*':
+		else/*If its have a operands*/
 		{
-		    	*dstType=4;
-			strcpy(*dstName, (str+2));  
-			break;  
-		}
- 		
-		default :
-		{
-
-			*dstType=4;
-			if(*str == 'r')
+			comma_logic(&line);/*Checks if its have a comma*/
+			skip_spaces(&line);
+			switch(*str)/*Switch that checks the first operand type and copy the type to dstName*/
 			{
-				str++;
-				if ((atoi(str) < 8) && (atoi(str) > 0))
+				case '#':/*If is direct address-initializes the srcType accordingly*/
 				{
-					if(*(str+1) == '\0')
+					*srcType = IMMEDIATE;
+					strcpy(*srcName, (str+1));
+					break;
+				}
+
+				case '*':/*If is indirect address-initializes the srcType accordingly*/
+				{	
+
+				    	*srcType = INDIRECT_REGISTER;
+					strcpy(*srcName, (str+2));
+					break;
+				}
+		 		
+				default:
+				{
+					if(*str == 'r')/*If is direct register address-initializes the srcType accordingly*/
 					{
-						*dstType = 8;
-						strcpy(*dstName, str);
+						str++;
+						if ((atoi(str) < MAX_REGISTER) && (atoi(str) > MIN_REGISTER))
+						{
+							if(*(str+1) == '\0')
+							{
+								*srcType = DIRECT_REGISTER;
+								strcpy(*srcName, str);
+							}
+						}
 					}
+					else/*If is not one of the previous cases*/
+					{
+						if (loop == SECOND)
+							label = get_label(str, NOTHING, NOTHING, label);
+						if((label!=NULL)||(loop == FIRST))/*Is label*/
+						{
+							*srcType = DIRECT;
+							strcpy(*srcName, str);
+						}
+						else/*If is not of the previous cases,error*/
+						{
+							add_error(INVALID_SRC_TYPE);
+						}
+					}
+			
+					break;
 				}
 			}
-			else
+			memset(str, '\0', strlen(str));
+			pch=(strpbrk(line," \t\n,"));
+			strncpy(str,line,(pch-line));
+			line=pch;
+		}
+		switch(*str)/*A same switch that checks the secand operand type and copy the type to dstName*/
+		{
+			case '#':
 			{
-				if (loop == 2)				
-					get_label(str, 0, 0, &label);	
-
-				if((label!=NULL)||(loop == 1))
+				*dstType = IMMEDIATE;
+				strcpy(*dstName, (str+1));		
+				break;
+			}
+			case '*':
+			{
+			    	*dstType= INDIRECT_REGISTER;
+				strcpy(*dstName, (str+2));  
+				break;  
+			}		 		
+			default :
+			{
+				if(*str == 'r')
 				{
-					*dstType = 2;
-					strcpy(*dstName, str);
+					str++;
+					if ((atoi(str) < MAX_REGISTER) && (atoi(str) > MIN_REGISTER))
+					{
+						if(*(str+1) == '\0')
+						{
+							*dstType = DIRECT_REGISTER;
+							strcpy(*dstName, str);
+						}
+					}
 				}
 				else
 				{
-					add_error(INVALID_DST_TYPE);
+					if (loop == SECOND)				
+						label = get_label(str, NOTHING, NOTHING, label);	
+					if((label!=NULL)||(loop == FIRST))
+					{	
+						*dstType = DIRECT;
+						strcpy(*dstName, str);
+					}
+					else
+					{
+						add_error(INVALID_DST_TYPE);
+					}
 				}
+		
+				break;
 			}
-			break;
-			
 		}
+		legal_EOL(&line);
+		/*free(str);*/
 	}
-	legal_EOL(&line);
+	
 }
 
 

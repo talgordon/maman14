0a27686db757cfeaed215476012577b61b2486b9
talgordon <talgordon0@gmail.com>
2020-03-27 07:27:19 -0400

    final

--- analize_input_line.h
+++ analize_input_line.h
@@ -1,6 +1,18 @@
 #include <ctype.h>
 #define MAX_WORD 80
-#define MAX_LINE 127	
+#define MAX_LINE 127
+#define NO_OPERAND 0
+#define IMMEDIATE 1
+#define DIRECT 2
+#define INDIRECT_REGISTER 4
+#define DIRECT_REGISTER 8
+#define FIRST 1
+#define SECOND 2
+#define MAX_REGISTER 8
+#define MIN_REGISTER 0
+#define NUM_OF_ADDRESS_METHOD 4
+#define NUM_OF_OPCODE 16
+	
 unsigned int DC;
 unsigned int IC;
 unsigned int L;
@@ -12,11 +24,11 @@ typedef enum type{LABEL , DATA , STRING , EXTERN , ENTRY, CODE, END} types;
 typedef struct addressMethod *addrMethodPtr;
 
 typedef struct addressMethod{
-	unsigned int src : 4;
-	unsigned int dst : 4;
+	unsigned int src : NUM_OF_ADDRESS_METHOD;
+	unsigned int dst : NUM_OF_ADDRESS_METHOD;
 }addressMethod;
 
-addressMethod addressTable[16];
+addressMethod addressTable[NUM_OF_OPCODE];
 
 /*A function that skip white spaces.*/
 void skip_spaces(char **line);
@@ -24,8 +36,6 @@ void skip_spaces(char **line);
 void legal_EOL(char ** line);
 /*A function that checks for a comma or not and advances the line accordingly*/
 void comma_logic(char **line);
-/*A function that initializes global variables using code*/
-void init();
 /*A function that breaks down an input line and returns the following data every time*/
 int get_data(char **line);
 /*A function that check if the line is label and return 0/1*/
